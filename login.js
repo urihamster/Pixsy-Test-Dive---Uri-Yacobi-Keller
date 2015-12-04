a2401321e9af22d9ce433fecbbc8cafe7caf0a97
urihamster <uri.h.y.k@gmail.com>
2015-12-04 05:42:32 +0100

    everything

---
+++ login.js
@@ -0,0 +1,30 @@
+var react = require('react/addons');
+
+var loginReact = react.createClass({
+
+	render: function(){
+		return (
+
+			<div> 
+			<form>
+					Email:<br />
+				 	
+			</form>
+
+			</div>
+
+			)
+	}
+
+
+});
+
+/*
+<input type="text" name="email" />
+				  	<br>
+					Password:
+					<br>
+				  	<input type="text" name="password" />
+				  	*/
+
+module.exportys=loginReact;
